dbfffdd5991c0d5b9083860deb8890d30024673c
AMAN KUMAR JHA <95953071+Aman4443@users.noreply.github.com>
2026-01-13 21:09:24 +0530

    Spring Boot Ud

---
+++ learn-spring-Boot/src/test/java/com/in28min/springBoot/learn_spring_Boot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28min.springBoot.learn_spring_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
